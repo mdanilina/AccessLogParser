e90bb46a6a0288061077c5834bc53cec53064d7c
mdanilina <mdanilina@datatech.ru>
2025-06-26 17:00:06 +0300

    home_work Синтаксис языка и Базовые типы данных 2

--- src/main/java/ru/stepup/payments/MainApplication.java
+++ src/main/java/ru/stepup/payments/MainApplication.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class MainApplication {
     public static <string> void main(String[] args) {
         Scanner in1 = new Scanner(System.in);
-        System.out.println("введите первое число:");
+        System.out.println("Введите первое число:");
         int firstNumber = in1.nextInt();
 
         Scanner in2 = new Scanner(System.in);
-        System.out.println("введите второе число:");
+        System.out.println("Введите второе число:");
         int secondNumber = in2.nextInt();
 
-        System.out.println("сумма чисел: " + (firstNumber + secondNumber));
-        System.out.println("разность чисел: " + (firstNumber - secondNumber));
-        System.out.println("произведение чисел: " + (firstNumber * secondNumber));
+        System.out.println("Сумма чисел: " + (firstNumber + secondNumber));
+        System.out.println("Разность чисел: " + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел: " + (firstNumber * secondNumber));
 
         double quotient = (double) firstNumber / secondNumber;
-        System.out.println("частное чисел: " + quotient);
+        System.out.println("Частное чисел: " + quotient);
         }
     }
