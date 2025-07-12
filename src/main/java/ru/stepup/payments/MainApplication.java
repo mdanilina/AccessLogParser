58600181d4879391e1bc47a7dc64e6b4063b1c5b
mdanilina <mdanilina@datatech.ru>
2025-07-12 16:13:25 +0300

    Курсовой проект. Задание по теме "Циклы"

--- src/main/java/ru/stepup/payments/MainApplication.java
+++ src/main/java/ru/stepup/payments/MainApplication.java
@@ -1,7 +1,32 @@
 package ru.stepup.payments;
 
+import java.io.File;
 import java.util.Scanner;
 
+public class MainApplication {
+    public static <string> void main(String[] args) {
+        int y = 0;
+        int x = 10;
+        while (x >= 10){
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if ((!fileExists) || (isDirectory)) {
+                System.out.println("указанный файл не существует или указанный путь является путём к папке");
+                continue;
+            }
+            else {
+                if ((!isDirectory) && (fileExists)) {
+                    y++;
+                    System.out.println("Путь указан верно");
+                    System.out.println("Это файл номер "+y);
+                }
+            }
+        }
+    }
+    }
+/*
 public class MainApplication {
     public static <string> void main(String[] args) {
         Scanner in1 = new Scanner(System.in);
@@ -14,9 +39,10 @@ public class MainApplication {
 
         System.out.println("Сумма чисел: " + (firstNumber + secondNumber));
         System.out.println("Разность чисел: " + (firstNumber - secondNumber));
-        System.out.println("Произведение чисел: " + (firstNumber * secondNumber));
+        System.out.println("Произведениеит889зщш чисел: " + (firstNumber * secondNumber));
 
         double quotient = (double) firstNumber / secondNumber;
         System.out.println("Частное чисел: " + quotient);
         }
     }
+ */
