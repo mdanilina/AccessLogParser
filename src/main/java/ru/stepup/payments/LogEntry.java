0089070dd017b167bf01a2f1aa5b5520ddfc1b76
mdanilina <mdanilina@datatech.ru>
2025-09-29 14:30:19 +0300

    Задание #2-3. Сравнение обьектов и введение в ООП 29092025

--- src/main/java/ru/stepup/payments/LogEntry.java
+++ src/main/java/ru/stepup/payments/LogEntry.java
@@ -1,32 +1,40 @@
 package ru.stepup.payments;
 
+import lombok.Data;
+
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+@Data
 public class LogEntry {
 
-    private final String ipAddr, str, method, path, responseCode, referer, userAgent;
-    private final Integer responseSize;
+    private final String ipAddr, str, referer, userAgent;
+    private final String path;
+    private final Integer responseCode;
+    private final Long responseSize;
+
     //private final Integer responseSize;
-    private final Instant time;
+    private final LocalDateTime time;
+    private final Method method;
 
     //211.71.205.41 - - [25/Sep/2022:06:25:08 +0300] "GET /november-reports/estimation/decisions/6373/67 HTTP/1.0" 200 9273 "-" "Mozilla/5.0 (compatible; MegaIndex.ru/2.0; +http://megaindex.com/crawler)"
-    private final static String DELEMETER = " ";
-    private final static int ipAddr_index=0;
-    private final static int str_index1=1;
-    private final static int str_index2=2;
-    private final static int time_index=3;
-    private final static int time_index1=4;
-    private final static int method_index=5;
-    private final static int path_index=6;
-    private final static int responseCode_index=8;
-    private final static int responseSize_index=9;
-    private final static int referer_index=10;
-    private final static int userAgent_index=11;
-    private final static int userAgent_index1=12;
-    private final static int userAgent_index2=13;
-
+    private final String DELEMETER = " ";
+    private final int ipAddr_index = 0;
+    private final int str_index1 = 1;
+    private final int str_index2 = 2;
+    private final int time_index = 3;
+    private final int time_index1 = 4;
+    private final int method_index = 5;
+    private final int path_index = 6;
+    private final int responseCode_index = 8;
+    private final int responseSize_index = 9;
+    private final int referer_index = 10;
+    private final int userAgent_index = 11;
+    private final int userAgent_index1 = 12;
+    private final int userAgent_index2 = 13;
+    private final int userAgent_index3 = 14;
 
 
     public LogEntry(String line) {
@@ -34,78 +42,28 @@ public class LogEntry {
         String[] parts = line.split(DELEMETER);
 
         this.ipAddr = parts[ipAddr_index];
-        this.str = parts[str_index1]+ " " +parts[str_index2];
-        this.time = gettime(parts[time_index]+" " + parts[time_index1]);
-        this.method = parts[method_index].substring(1,parts[method_index].length());
+        this.str = parts[str_index1] + " " + parts[str_index2];
+        this.time = get_time(parts[time_index] + " " + parts[time_index1]);
+        this.method = Method.valueOf(parts[method_index].replace("\"",""));//parts[method_index].length()+1));
+        //this.path = Path.of(parts[path_index]);
         this.path = parts[path_index];
-        this.responseCode = parts[responseCode_index];
-        this.responseSize = Integer.parseInt(parts[responseSize_index]);
+        this.responseCode = Integer.parseInt(parts[responseCode_index]);
+        this.responseSize = Long.valueOf(parts[responseSize_index]);
         this.referer = parts[referer_index];
         //System.out.println(parts.length);
 
-        String str="";
-        for(int i=11;i<parts.length;i++){
-            str+=parts[i];
+        String str = "";
+        for (int i = 11; i < parts.length; i++) {
+            str += parts[i];
         }
         this.userAgent = str;
-        /*if (parts.length == 15 ) {
-            this.userAgent = parts[userAgent_index] + parts[userAgent_index1] + parts[userAgent_index2];
-        }
-        else if (parts.length == 14 ) {
-            this.userAgent = parts[userAgent_index] + parts[userAgent_index1];
-        }
-        else {
-            this.userAgent = parts[userAgent_index];
-        }*/
 
     }
 
     //Передаем дататайм в кв скобках [25/Sep/2022:06:25:08 +0300]
-    private static Instant gettime(String str) {
-        String str_new = str.substring(1,str.length()-1);
+    private LocalDateTime get_time(String str) {
+        String str_new = str.substring(1, str.length() - 1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
-        Instant instant1 = formatter.parse(str_new, Instant::from);
-        return instant1;
-    }
-    public String getIP() {
-        return ipAddr;
-    }
-
-    public String getStr() {
-        return str;
-    }
-
-    public Instant getDatetime() {
-        return time;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    @Override
-    public String toString(){
-        return "Log Entry "+ "{"+this.ipAddr + " " + this.str +" "+  this.time + " " + this.method + " " + this.path + " " + this.responseCode + " " + this.responseSize
-        + " " + this.referer + " " + this.userAgent + "}";
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public String getCode() {
-        return responseCode;
-    }
-
-    public String getReferer() {
-        return referer;
-    }
-
-    public String getUserAgent() {
-        return userAgent;
-    }
-
-    public Integer getResponseSize() {
-        return responseSize;
+        return formatter.parse(str_new, LocalDateTime::from);
     }
 }
--- src/main/java/ru/stepup/payments/Statistics.java
+++ src/main/java/ru/stepup/payments/Statistics.java
@@ -1,29 +1,43 @@
 package ru.stepup.payments;
 
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class Statistics {
-    private Integer totalTraffic;
-    LogEntry logEntry;
-    LocalDateTime ldt;
-    //private List<Integer> stat=new ArrayList<>();
+    private Long totalTraffic = 0L;
+    private List<LogEntry> logEntry = new ArrayList<>();
+    private LocalDateTime minTime, maxTime;
+    private Integer totalRequests = 0;
 
-    public Statistics(LogEntry logEntry, LocalDateTime ldt) {
-        this.logEntry = logEntry;
-        this.ldt = ldt;
+    public void addEntry(LogEntry logEntry) {
+        LocalDateTime time = logEntry.getTime();
+        if (minTime == null || time.isBefore(minTime)) {
+            minTime = time;
+        }
+        if (maxTime == null || time.isAfter(maxTime)) {
+            maxTime = time;
+        }
+        totalRequests++;
+        if (logEntry.getResponseSize() != null && logEntry.getResponseSize() > 0) {
+            totalTraffic += logEntry.getResponseSize();
+        }
+        this.logEntry.add(logEntry);
     }
 
-    public void addEntry(int totalTraffic) {
-        this.totalTraffic = totalTraffic;
-    }
-
-    @Override
-    public String toString() {
-        return "Statistica " + "{" + this.logEntry + " " + this.totalTraffic + " " + this.ldt + "}";
-    }
-
-    public Integer getTotalTraffic() {
-        return totalTraffic;
+    public Long getTrafficRate() {
+        if (minTime == null || maxTime == null) {
+            return 0L;
+        }
+        long hours = Duration.between(minTime, maxTime).toHours();
+        if (hours == 0) {
+            return totalTraffic;
+        }
+        return totalTraffic / hours;
     }
 }
